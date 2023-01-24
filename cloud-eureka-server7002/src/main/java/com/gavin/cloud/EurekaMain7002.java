07b102f735bf883beec46ed23006193ce286129d
Gavin <805792578@qq.com>
2023-01-24 23:02:41 +0800

    eureka Server cluster

--- cloud-eureka-server7002/src/main/java/com/gavin/cloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/gavin/cloud/EurekaMain7002.java
@@ -5,10 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer //一定要标明是Eureka服务端
-public class EurekaMain7001 {
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaMain7001.class, args);
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args){
+        SpringApplication.run(EurekaMain7002.class, args);
     }
-
 }
