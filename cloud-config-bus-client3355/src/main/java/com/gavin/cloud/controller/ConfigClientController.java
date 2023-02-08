3215f49155929803bbebac72781c983b5d35d166
Gavin <805792578@qq.com>
2023-02-08 22:39:02 +0800

    Adding Spring Cloud Bus

---
+++ cloud-config-bus-client3355/src/main/java/com/gavin/cloud/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package com.gavin.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//接口以REST风格
+@RestController
+@RefreshScope //Config客户端支持动态刷新
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
